d92b51b805ce1dc7eb1f5a166dc712d8086b68ab
maurigvs <maurigvs@icloud.com>
2023-11-25 21:13:59 +0000

    initial commit

---
+++ src/test/java/br/com/maurigvs/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
