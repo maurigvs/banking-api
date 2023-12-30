08b0081f19332b3bda6325f22d5bfa15eb2e22bc
maurigvs <maurigvs@icloud.com>
2023-12-30 22:34:12 +0000

    account-holder module

---
+++ account-holder/src/main/java/com/maurigvs/bank/accountholder/AccountHolderApplication.java
@@ -0,0 +1,13 @@
+package com.maurigvs.bank.accountholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountHolderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountHolderApplication.class, args);
+    }
+
+}
--- src/main/java/br/com/maurigvs/banking/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package br.com.maurigvs.banking.exception;
-
-public class BusinessException extends Exception {
-
-    public BusinessException(String message){
-        super(message);
-    }
-}
