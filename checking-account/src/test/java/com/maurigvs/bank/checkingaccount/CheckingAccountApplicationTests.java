2f4b252c4184dbdbfb905e2f8a7a91a39764b72b
maurigvs <maurigvs@icloud.com>
2024-01-01 18:37:25 +0000

    checking account service initial commit

---
+++ checking-account/src/test/java/com/maurigvs/bank/checkingaccount/CheckingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurigvs.bank.checkingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
