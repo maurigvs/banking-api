51ea7e9a42affbdfc0c25b1180bb2f66cd3ac8ca
maurigvs <maurigvs@icloud.com>
2024-01-21 14:51:26 +0000

    Revert "customer service"
    This reverts commit 3772697438c1bd48b920e2dd494076d907f408ee.

--- customers/src/main/java/com/maurigvs/bank/customers/model/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.maurigvs.bank.customers.model.exception;
-
-public class BusinessException extends Exception {
-
-    public BusinessException(String message) {
-        super(message);
-    }
-}
