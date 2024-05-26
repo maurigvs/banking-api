e7ecb6deff475f3723c0c3eae3b81bf8c94853ce
maurigvs <maurigvs@icloud.com>
2024-05-26 12:25:46 +0100

    account-api: service

--- account-api/src/main/java/br/maurigvs/banking/account/service/AccountService.java
+++ account-api/src/main/java/br/maurigvs/banking/account/service/AccountService.java
@@ -1,12 +1,9 @@
 package br.maurigvs.banking.account.service;
 
 import br.maurigvs.banking.account.model.Account;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface AccountService {
 
     Mono<Account> create(Account account);
-
-    Flux<Account> findAll();
 }
--- account-api/src/main/java/br/maurigvs/banking/account/service/AccountServiceImpl.java
+++ account-api/src/main/java/br/maurigvs/banking/account/service/AccountServiceImpl.java
@@ -4,7 +4,6 @@ import br.maurigvs.banking.account.model.Account;
 import br.maurigvs.banking.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -12,17 +11,11 @@ import reactor.core.scheduler.Schedulers;
 @RequiredArgsConstructor
 class AccountServiceImpl implements AccountService {
 
-    private final AccountRepository accountRepository;
+    private final AccountRepository repository;
 
     @Override
     public Mono<Account> create(Account account) {
-        return Mono.fromSupplier(() -> accountRepository.save(account))
-                .subscribeOn(Schedulers.boundedElastic());
-    }
-
-    @Override
-    public Flux<Account> findAll() {
-        return Flux.fromStream(accountRepository.findAll().stream())
+        return Mono.fromSupplier(() -> repository.save(account))
                 .subscribeOn(Schedulers.boundedElastic());
     }
 }
